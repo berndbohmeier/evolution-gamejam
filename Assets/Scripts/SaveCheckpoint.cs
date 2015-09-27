5e52ae821b7d8ba09e730430bc4578a624851cfb
Bernd Bohmeier <berndbohmeier@googlemail.com>
2015-09-27 11:53:56 -0600

    added checkpoint

---
+++ Assets/Scripts/SaveCheckpoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SaveCheckpoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Vector3 pos = GetComponent<Transform>().position;
+            other.gameObject.GetComponent<DeadHandler>().SetRespawn(pos.x,pos.y);
+        }
+
+    }
+}
